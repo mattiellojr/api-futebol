10a43579a3c3167c0728b13970ad5666e0d6df88
Mauricio de Azevedo <mmadrugadeazevedo@gmail.com>
2017-04-07 00:37:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 console.log(ufcData);
 
-
-$.ajax({
-	url: 'http://api.football-data.org/v1/competitions',
-	dataType: 'json',
-	type: 'GET',
-}).done(function(response) {
-	console.log(response);
-});
-
 $.ajax({
   headers: { 'X-Auth-Token': 'a48fda37e7f64616a1b61b61b3b66cfc' },
   url: 'http://api.football-data.org/v1/competitions',
